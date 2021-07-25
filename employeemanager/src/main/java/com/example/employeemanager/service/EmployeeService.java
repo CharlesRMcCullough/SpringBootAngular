76add2e76f11aa7b9cf32b5a7c9afb2d8a282d03
Charles McCullough <charles.mccullough@gmail.com>
2021-07-25 17:15:30 -0500

    Initial Commit

---
+++ employeemanager/src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.example.employeemanager.service;
+
+import com.example.employeemanager.repo.EmployeeRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+
+
+    
+}
